f2ce7829575caf485751c5c771bd5a04a01eea3c
олег <@DESKTOP-SSTKF2A>
2023-04-02 13:15:39 +0300

    Добавьте файлы проекта.

---
+++ decorrr/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace decorrr
+{
+    abstract class Rain
+    {
+        public Rain(string n)
+        {
+            this.Name = n;
+        }
+        public string Name { get; protected set; }
+        public abstract int GetCost();
+    }
+
+    class HeavyRain : Rain
+    {
+        public HeavyRain() : base("Сильний дощ")
+        { }
+        public override int GetCost()
+        {
+            return 10;
+        }
+    }
+    class LightRain : Rain
+    {
+        public LightRain()
+            : base("Легкий дощ")
+        { }
+        public override int GetCost()
+        {
+            return 8;
+        }
+    }
+
+    abstract class RainDecorator : Rain
+    {
+        protected Rain rain;
+        public RainDecorator(string n, Rain rain) : base(n)
+        {
+            this.rain = rain;
+        }
+    }
+
+    class IceRain : RainDecorator
+    {
+        public IceRain(Rain p)
+            : base(p.Name + ", з градом", p)
+        { }
+
+        public override int GetCost()
+        {
+            return rain.GetCost() + 5;
+        }
+    }
+
+    class ThunderstormRain : RainDecorator
+    {
+        public ThunderstormRain(Rain p)
+            : base(p.Name + ", з грозою", p)
+        { }
+
+        public override int GetCost()
+        {
+            return rain.GetCost() + 5;
+        }
+    }
+}
---
+++ decorrr/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace decorrr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Rain rain1 = new HeavyRain();
+            rain1 = new IceRain(rain1); 
+            MessageBox.Show("Назва:"+ rain1.Name);
+            MessageBox.Show("Кількість опадів:"+ rain1.GetCost());
+
+            Rain rain2 = new HeavyRain();
+            rain2 = new ThunderstormRain(rain2);
+            MessageBox.Show("Назва:"+ rain2.Name);
+            MessageBox.Show("Кількість опадів:"+ rain2.GetCost());
+
+            Rain rain3 = new LightRain();
+            rain3 = new IceRain(rain3);
+            rain3 = new ThunderstormRain(rain3);
+            MessageBox.Show("Назва:"+ rain3.Name);
+            MessageBox.Show("Кількість опадів:"+ rain3.GetCost());
+        }
+    }
+}
